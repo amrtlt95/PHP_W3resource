b8bf13f5206ed92f529b616cddafe27241592600
Amr Talaat <amrtlt90@gmail.com>
2025-01-09 01:57:33 +0200

    example 2 solution

---
+++ W3resource/Basic/2.php
@@ -0,0 +1,16 @@
+<?php
+/*
+2. Write a PHP script to display the following strings.
+
+Sample String :
+'Tomorrow I \'ll learn PHP global variables.'
+'This is a bad command : del c:\\*.*'
+
+Expected Output :
+Tomorrow I 'll learn PHP global variables.
+This is a bad command : del c:\*.* 
+*/
+echo "Tomorrow I \'ll learn PHP global variables.";
+echo "</br>";
+echo "This is a bad command : del c:\\*.*";
+?>
