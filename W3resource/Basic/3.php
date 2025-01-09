88f5aba75fc77aa3748e19c1e0c7e0af7e94ddce
Amr Talaat <69372602+amrtlt95@users.noreply.github.com>
2025-01-09 18:48:49 +0200

    solved third challange

---
+++ W3resource/Basic/3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<?php $var = 'PHP Tutorial';?>
+<head>
+	<title>
+    	<?= $var;?>
+    </title>
+</head>
+<body>
+
+<h3><?= $var; ?></h3>
+PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.
+Go to the <a href="#"><?= $var; ?></a>.
+
+</body>
+</html>
